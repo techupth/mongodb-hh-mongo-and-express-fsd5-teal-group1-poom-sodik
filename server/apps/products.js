3fd436833d0bf3ce0af9f2c5a3f4d0feae927d1b
Sodikcp <paramesh.rah@gmail.com>
2024-01-12 15:20:58 +0700

    feat:complete Mongo And Express

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,62 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import { ObjectId } from "mongodb";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  const collection = db.collection("products");
+  const products = await collection.find({}).toArray();
+  return res.json({
+    data: products,
+  });
+});
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  let productId = new ObjectId(req.params.id);
+  const product = await collection.findOne({ _id: productId });
+  return res.json({
+    data: product,
+    message: "send",
+  });
+});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const collection = db.collection("products");
+  const productsData = { ...req.body };
+  const products = await collection.insertOne(productsData);
+  return res.json({
+    message: "Product has been created successfully",
+  });
+});
 
-productRouter.put("/:id", (req, res) => {});
+productRouter.put("/:id", async (req, res) => {
+  const collection = db.collection("products");
 
-productRouter.delete("/:id", (req, res) => {});
+  let productId = new ObjectId(req.params.id);
+  const newProductsData = { ...req.body };
+
+  const product = await collection.updateOne(
+    {
+      _id: productId,
+    },
+    {
+      $set: newProductsData,
+    }
+  );
+  return res.json({
+    message: "Product has been updated successfully",
+  });
+});
+
+productRouter.delete("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  let productId = new ObjectId(req.params.id);
+  await collection.deleteOne({ _id: productId });
+  return res.json({
+    message: "Product has been deleted successfully",
+  });
+});
 
 export default productRouter;
